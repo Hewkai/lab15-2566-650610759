90b1979d46289dcafbab2a0f590c3d22a0614e40
Hewkai <fkgthnotv@gmail.om>
2023-08-31 15:33:48 +0700

    lab15

--- src/app/page.js
+++ src/app/page.js
@@ -44,9 +44,18 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z
+      .string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters " }),
     confirmPassword: z.string(),
   })
+  .refine(
+    (data) => {
+      if (data.password != data.confirmPassword) return false;
+    },
+    { message: "Password does not match", path: ["confirmPassword"] }
+  )
   .refine(
     //refine let you check error in your own way
     //in this example, we check "hasCoupon" with "coupon" fields
@@ -89,7 +98,14 @@ export default function Home() {
 
     //TIP : get value of currently filled form with variable "form.values"
 
-    if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "funrun") price += 500;
+    if (form.values.plan === "mini") price += 800;
+    if (form.values.plan === "half") price += 1200;
+    if (form.values.plan === "full") price += 1500;
+
+    if (form.values.coupon === "CMU2023") {
+      price -= price * 0.3;
+    }
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
@@ -175,7 +191,11 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year={2023}
+          fullName="Nattida Phaiboontanasan"
+          studentId="650610759"
+        />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
